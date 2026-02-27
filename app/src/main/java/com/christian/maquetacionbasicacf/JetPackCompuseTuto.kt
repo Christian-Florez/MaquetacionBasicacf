f54232ed2984d34c2390874faaa8ab2e7171b88d
Christian-Florez <cflorez669@unab.edu.co>
2026-02-26 23:30:15 -0500

    Jetpackcommit

--- app/src/main/java/com/christian/maquetacionbasicacf/JetPackCompuseTuto.kt
+++ app/src/main/java/com/christian/maquetacionbasicacf/JetPackCompuseTuto.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
